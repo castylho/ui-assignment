a9b77b3a45d1bac2a080a61bfd65467aec4a42d8
Yohann Castilho <ygoncalves@alter-solutions.com>
2022-10-24 10:02:23 +0100

    messages && custom hook

---
+++ src/messages.js
@@ -0,0 +1,10 @@
+const messages = {
+  title: "Account Overview",
+  sales: "Sales",
+  supportContactTitle: "YOUR FEEFO SUPPORT CONTACT",
+  supportPhoneNumber: "020 3362 4208",
+  supportDefaultEmail: "support@feefo.com",
+  supportDefaultName: "Support",
+};
+
+export default messages;
